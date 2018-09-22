e94e06ece1cf7cb588dced17e85c4bfd36902acd
Bruno <sitespomerode@gmail.com>
2018-09-21 21:33:14 -0500

    new

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Simple Laravel VueJs</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="github, php, webdeveloper, codeigniter, laravel, jquery, vueJs,vue,programmer, web designer, developer, mysql"/>
+    <meta name="author" content="brunolc.site"/>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta property="description" content="Little test with Laravel and VueJs"/>
+    <meta property="og:title" content="Laravel and VueJS"/>
+    <meta property="og:description" content="Little test with Laravel and VueJs"/>
+    <meta property="op:markup_url" content="http://brunolc.site">
+    <meta property="og:type" content="website" /> 
+    <meta property="og:image" content="https://avatars3.githubusercontent.com/u/12675160?s=460&v=4"/>
     <link rel="stylesheet" type="text/css" media="screen" href="{{ asset('public/css/main.css') }}" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
