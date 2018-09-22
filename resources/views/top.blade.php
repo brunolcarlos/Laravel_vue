d94ad3786691029131e39fdf6deacb97219ecd20
Bruno <sitespomerode@gmail.com>
2018-09-21 22:53:22 -0500

    visual

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,7 +1,7 @@
 <nav>
     <ul class="nav nav-tabs">
         <li class="nav-item">
-            <a class="nav-link active" href="{{asset('/')}}">Home</a>
+            <a class="nav-link active" href="{{asset('/')}}">Home Page</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="{{asset('/users_laravel')}}">Users with Laraval</a>
